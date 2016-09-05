002acae7307de412e1a36b30a12b73cf1e5589f0
justed <dev.justed@gmail.com>
2016-09-05 23:02:23 +0500

    bad way to implement hover

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -6,14 +6,29 @@ let NavItem = React.createClass({
     title: React.PropTypes.string,
     aStyle: React.PropTypes.object
   },
+  getInitialState: function () {
+    return {
+      hover: false
+    }
+  },
   render: function () {
     return (
-      <li>
+      <li
+        onMouseOver={this.mouseOver}
+        onMouseOut={this.mouseOut}
+        className={this.state.hover ? 'active' : ''}
+      >
         <a href={this.props.href} style={this.props.aStyle}>
           {this.props.title}
         </a>
       </li>
     )
+  },
+  mouseOver: function () {
+    this.setState({hover: true})
+  },
+  mouseOut: function () {
+    this.setState({hover: false})
   }
 })
 
